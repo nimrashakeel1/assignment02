f7d90de733ec73e8899b5e3d8ee69ec7025244f0
nimrashakeel1 <nimzakhan86@gmail.com>
2024-11-05 20:05:04 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        colors: {
+          'custom-red': '#e74040',
+          'custom-gray': '#737373',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
+
